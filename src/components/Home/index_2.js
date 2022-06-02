092a8320404d959a44111590c9ba4c614b0c9dfd
Alok Dhiman <alokdhiman@Aloks-MacBook-Air.local>
2022-06-03 00:20:18 +0530

    gyro added

--- src/components/Home/index_2.js
+++ src/components/Home/index_2.js
@@ -5,11 +5,14 @@ import {useEffect ,useState} from 'react';
 
 const Test = () => {
     const [accData, setAccData] = useState({x:0, y:0, z:0});
+    const [gyro , setGyro] = useState(false);
     function handler(event){
-        let x = event.acceleration.x;
-        let y = event.acceleration.y;
-        let z = event.acceleration.z;
-        setAccData({x,y,z});
+        if(window.DeviceOrientationEvent){
+            setGyro(true);
+            window.addEventListener('deviceorientation', function(event){
+                setAccData({x:event.gamma, y:event.beta, z:event.alpha});
+            });
+        }
     }
     useEffect(() => {
         window.addEventListener('devicemotion', handler);
@@ -20,6 +23,7 @@ const Test = () => {
     
     return (
     <div>
+        <h1>Gyro supported : {gyro ? "Yes" :"No" }</h1>
         <h1>x: {accData.x}</h1>
         <h1>y: {accData.y}</h1>
         <h1>z: {accData.z}</h1>
